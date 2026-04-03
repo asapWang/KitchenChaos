0ec957b4e969c46d23e10507b5396ef1aef45b52
asapWang <143229976+asapWang@users.noreply.github.com>
2026-04-03 19:37:30 +0800

    选项面板：按键重绑定

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem;
 
 public class InputSystem : MonoBehaviour
 {
     public static InputSystem Instance;
-    
     private InputActions inputActions;
+    //定义常量，表示按键绑定信息的键名
+    private const string Player_Input_Rebinds = "PlayerInputRebinds";
     //与ClearCounter交流事件
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternativeAction;
     public event EventHandler OnPauseAction;
+    //键位名称枚举
+    public enum Binding
+    {
+        Move_Up,
+        Move_Down,
+        Move_Left,
+        Move_Right,
+        Interact,
+        Interact_Alternate,
+        Pause
+    }
     void Awake()
     {
         Instance = this;
         inputActions = new InputActions();
+        //加载保存的按键绑定信息，如果有的话
+        if (PlayerPrefs.HasKey(Player_Input_Rebinds))
+        {
+            //InputActions有一个特别方便的方法，LoadBindingOverridesFromJson()方法会把保存的JSON字符串加载到输入动作中，恢复之前的绑定设置
+            inputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(Player_Input_Rebinds));
+        }
         inputActions.Enable();
         //事件的好处就是解耦合，并且不需要每一帧都检测，但是新输入系统的自带事件，底层其实还是每帧检测，只不过在我们代码层面简化了不需要考虑这些
         inputActions.Player.Interact.performed += AllowInteraction;
@@ -36,17 +55,97 @@ public class InputSystem : MonoBehaviour
         return movePosition;
     }
 
-    private void AllowInteraction(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void AllowInteraction(InputAction.CallbackContext context)
     {
         OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
-    private void AllowInteractionAlternative(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void AllowInteractionAlternative(InputAction.CallbackContext context)
     {
         OnInteractAlternativeAction?.Invoke(this, EventArgs.Empty);
     }
-    private void AllowPause(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void AllowPause(InputAction.CallbackContext context)
     {
         OnPauseAction?.Invoke(this, EventArgs.Empty);
     }
+    //获取按键绑定文本
+    public string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            default:
+            case Binding.Move_Up:
+            //ToDisplayString()方法会根据绑定的输入设备类型，只返回按键的名称，而不是完整的路径，这样UI显示更友好
+            //绑定的索引是单列索引，根据InputActions里面的顺序来的，下列四个的父对象WASD的索引是0
+                return inputActions.Player.Move.bindings[1].ToDisplayString();
+            case Binding.Move_Down:
+                return inputActions.Player.Move.bindings[2].ToDisplayString();
+            case Binding.Move_Left:
+                return inputActions.Player.Move.bindings[3].ToDisplayString();
+            case Binding.Move_Right:
+                return inputActions.Player.Move.bindings[4].ToDisplayString();
+            case Binding.Interact:
+                return inputActions.Player.Interact.bindings[0].ToDisplayString();
+            case Binding.Interact_Alternate:
+                return inputActions.Player.InteractAlternative.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return inputActions.Player.Pause.bindings[0].ToDisplayString();
+        }
+    }
+    //重新绑定按键
+    public void RebindBinding(Binding binding, Action onActionRebound)
+    {
+        //先禁用输入系统，防止在重新绑定过程中触发其他输入事件
+        inputActions.Player.Disable();
+        //根据绑定的枚举类型，找到对应的输入动作和绑定索引
+        InputAction inputAction;
+        int bindingIndex;
+        switch (binding)
+        {
+            default:
+            case Binding.Move_Up:
+                inputAction = inputActions.Player.Move;
+                bindingIndex = 1;
+                break;
+            case Binding.Move_Down:
+                inputAction = inputActions.Player.Move;
+                bindingIndex = 2;
+                break;
+            case Binding.Move_Left:
+                inputAction = inputActions.Player.Move;
+                bindingIndex = 3;
+                break;
+            case Binding.Move_Right:
+                inputAction = inputActions.Player.Move;
+                bindingIndex = 4;
+                break;
+            case Binding.Interact:
+                inputAction = inputActions.Player.Interact;
+                bindingIndex = 0;
+                break;
+            case Binding.Interact_Alternate:
+                inputAction = inputActions.Player.InteractAlternative;
+                bindingIndex = 0;
+                break;
+            case Binding.Pause:
+                inputAction = inputActions.Player.Pause;
+                bindingIndex = 0;
+                break;
+        }
 
+        // 开始重新绑定
+        inputAction.PerformInteractiveRebinding(bindingIndex)
+            .OnComplete(callback =>
+            {
+                callback.Dispose();
+                //保存新的绑定信息到PlayerPrefs，方便下次打开游戏时加载
+                //InputActions有一个特别方便的方法，SaveBindingOverridesAsJson()方法会把当前输入动作的所有绑定覆盖信息保存成一个JSON字符串
+                PlayerPrefs.SetString(Player_Input_Rebinds, inputActions.SaveBindingOverridesAsJson());
+                //重新启用输入系统
+                inputActions.Player.Enable();
+                // 重新绑定完成后，调用回调函数关闭提示UI，并且刷新显示的按键绑定文本
+                onActionRebound();
+            })
+            .Start();
+
+    }
 }
--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -6,11 +6,29 @@ using UnityEngine.UI;
 
 public class OptionsUI : MonoBehaviour
 {
+    //音量相关
     [SerializeField] private Slider musicVolumeSlider;
     [SerializeField] private Slider sfxVolumeSlider;
     [SerializeField] private TextMeshProUGUI musicVolumeValueText;
     [SerializeField] private TextMeshProUGUI sfxVolumeValueText;
+    //按键相关
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAltButton;
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private TextMeshProUGUI moveUpBindingText;
+    [SerializeField] private TextMeshProUGUI moveDownBindingText;
+    [SerializeField] private TextMeshProUGUI moveLeftBindingText;
+    [SerializeField] private TextMeshProUGUI moveRightBindingText;
+    [SerializeField] private TextMeshProUGUI interactBindingText;
+    [SerializeField] private TextMeshProUGUI interactAltBindingText;
+    [SerializeField] private TextMeshProUGUI pauseBindingText;
     [SerializeField] private Button closeButton;
+    //绑定按键提示UI
+    [SerializeField] private Transform pressToRebindKeyTransform;
     //引用audio mixer
     [SerializeField] private AudioMixer audioMixer;
     public static OptionsUI Instance { get; private set; }
@@ -20,10 +38,19 @@ public class OptionsUI : MonoBehaviour
         //监听音量滑动条的值改变事件
         musicVolumeSlider.onValueChanged.AddListener(MusicVolumeSlider_OnValueChanged);
         sfxVolumeSlider.onValueChanged.AddListener(SfxVolumeSlider_OnValueChanged);
-        closeButton.onClick.AddListener(() => Hide());
+        closeButton.onClick.AddListener(Hide);
+        //监听按键绑定按钮的点击事件
+        moveUpButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Move_Up));
+        moveDownButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Move_Down));
+        moveLeftButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Move_Left));
+        moveRightButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Move_Right));
+        interactButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Interact));
+        interactAltButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Interact_Alternate));
+        pauseButton.onClick.AddListener(() => PressRebindButton(InputSystem.Binding.Pause));
     }
     public void Start()
     {
+        //音量相关
         // 1. 拿到原始数据
         float savedMusicVol = DataMgr.Instance.SettingsData.musicVolume;
         float savedSfxVol = DataMgr.Instance.SettingsData.sfxVolume;
@@ -40,6 +67,10 @@ public class OptionsUI : MonoBehaviour
         audioMixer.SetFloat("Music", Mathf.Log10(savedMusicVol) * 20f);
         audioMixer.SetFloat("SFX", Mathf.Log10(savedSfxVol) * 20f);
 
+        //按键相关
+        UpdateVisual();
+        //一开始先把提示UI隐藏
+        pressToRebindKeyTransform.gameObject.SetActive(false);
         Hide();
     } 
     private void MusicVolumeSlider_OnValueChanged(float value)
@@ -61,6 +92,29 @@ public class OptionsUI : MonoBehaviour
         sfxVolumeValueText.text = (value * 100).ToString("F0");
         DataMgr.Instance.SettingsData.sfxVolume = value;
         DataMgr.Instance.SaveSettingsData();
+    }
+    private void UpdateVisual()
+    {
+        //按键相关
+        moveUpBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Move_Up);
+        moveDownBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Move_Down);
+        moveLeftBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Move_Left);
+        moveRightBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Move_Right);
+        interactBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Interact);
+        interactAltBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Interact_Alternate);
+        pauseBindingText.text = InputSystem.Instance.GetBindingText(InputSystem.Binding.Pause);
+    }
+    //按键绑定按钮的点击事件
+    private void PressRebindButton(InputSystem.Binding binding)
+    {
+        //显示提示UI
+        pressToRebindKeyTransform.gameObject.SetActive(true);
+        //重新绑定按键，绑定完成后更新UI显示，并隐藏提示UI
+        InputSystem.Instance.RebindBinding(binding, () =>
+        {
+            UpdateVisual();
+            pressToRebindKeyTransform.gameObject.SetActive(false);
+        });
     }
     public void Hide()
     {
